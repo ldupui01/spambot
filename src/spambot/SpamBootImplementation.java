71b5c53edee968b25e3723695897e25ef126a2f2
Ludo <ludo.dupuis@gmail.com>
2013-12-09 17:45:49 +0000

    crawler try 1

--- src/spambot/SpamBootImplementation.java
+++ src/spambot/SpamBootImplementation.java
@@ -1,27 +1,40 @@
 package spambot;
 import java.net.MalformedURLException;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
 
 public class SpamBootImplementation implements SpamBoot{
 
+	WebpageLinks weblink =  new WebpageLinks();
+	
 	@Override
 	public void setSeed(String seedUrl) throws MalformedURLException {
-			
-		
+		Webpage reader = new WebpageReader(seedUrl);
+		weblink.setUrls(reader.getLinks());
 	}
 
 	@Override
 	public String getSeed() {
-		// call url from datastore
+		// call next url from datastore
+		Set<String> s = weblink.getUrls();
+		for (Iterator<String> itr= s.iterator();itr.hasNext();itr.next()){
+			//String next = itr.next();
+			//System.out.println(itr.next());
+			try{
+				String newUrl = itr.next();
+				//this.setSeed(itr.next());	
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public void scanSite() {
 		// call method readpage with 
-		
 	}
 
 	@Override
@@ -32,25 +45,41 @@ public class SpamBootImplementation implements SpamBoot{
 
 	
 	public void launcher(){
-		Scanner keyboard = new Scanner(System.in);
+		/*Scanner keyboard = new Scanner(System.in);
 		System.out.println("enter the URL where to start the spamboot");
 		String url = keyboard.next();
+		*/
+		String url ="http://www.oracle.com";
+		
 		try{
 			this.setSeed(url);
 		}catch(Exception ex){
-				
 			System.out.println("something got wrong in the launcher");
+			ex.printStackTrace();
 		}
-		
+		this.getSeed();
 	}
 	
 	public static void main(String[] args){
 		// to know the number of processor
-		System.out.println(Runtime.getRuntime().availableProcessors());
-		
+		System.out.println("Processor: " +  Runtime.getRuntime().availableProcessors());
 		// Starting the spamboot
 		SpamBootImplementation spamBoot = new SpamBootImplementation();
 		spamBoot.launcher();
 	}
 	
+}
+
+class Crawler implements Runnable{
+	private Set<String> urlSet;
+	private Set<String> urlVisited;
+	private Set<String> emailSet;
+	
+	public Crawler(Set<String> urlSet){
+		this.urlSet = urlSet;
+	}
+	
+	public void run(){
+			
+	}
 }
